99183dc6b9db4e1c997e4f3d0db4afe9dd488d2e
alan890104 <alan890104@gmail.com>
2023-04-01 20:53:22 +0800

    fix: remove unused hardhat console

--- contracts/SafeUpgradeable.sol
+++ contracts/SafeUpgradeable.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "hardhat/console.sol";
 
 contract SafeUpgradeable {
     // Upgradeable pattern variables, check if contract is initialized
